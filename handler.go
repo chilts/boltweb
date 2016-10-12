55637f78fd564d4bf7fa427c1c7c8c3c1dd176bc
Andrew Chilton <andychilton@gmail.com>
2016-10-12 14:59:22 +1300

    Loop through top-level buckets

--- handler.go
+++ handler.go
@@ -23,4 +23,23 @@ type handler struct {
 
 func (h *handler) index(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "path = %s\n", r.URL.Path)
+
+	// pretend the path is currently just "/"
+	bucket := ""
+
+	if bucket == "" {
+		err := h.db.View(func(tx *bolt.Tx) error {
+			c := tx.Cursor()
+			for k, v := c.First(); k != nil; k, v = c.Next() {
+				fmt.Fprintf(w, "<li>%s : %s</li>\n", k, v)
+			}
+			return nil
+		})
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	} else {
+		fmt.Fprintf(w, "Deeper buckets are not yet implemented (%s)\n", r.URL.Path)
+	}
 }
